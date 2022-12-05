c21f7358ebc5d278f6828065d08d43f5ebb71c52
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-12-05 17:11:56 +0900

    Added `noindex,nofollow` in Head

--- Modules/Front/Resources/views/layouts/master.blade.php
+++ Modules/Front/Resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Front</title>
+        <meta name="robots" content="noindex,nofollow">
+        <title>{{ config('app.name') }}</title>
 
         <link rel="stylesheet" href="{{ asset('assets/front/css/app.css') }}">
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
