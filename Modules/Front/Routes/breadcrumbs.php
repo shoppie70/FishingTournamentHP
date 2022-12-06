0277d7c853c9ba653d8b3e9891cb9ce7fd562d53
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-12-06 11:44:36 +0900

    Added Breadcrumb Plugin.

---
+++ Modules/Front/Routes/breadcrumbs.php
@@ -0,0 +1,12 @@
+<?php
+// Home
+use Diglactic\Breadcrumbs\Breadcrumbs;
+
+Breadcrumbs::for('home', static function ($trail) {
+    $trail->push('トップページ', route('index'));
+});
+
+Breadcrumbs::for('about_us', static function ($trail, $title) {
+    $trail->parent('home');
+    $trail->push($title, route('about_us'));
+});
